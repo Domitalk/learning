d2a5c9df88805e988a34637c96eb1dfddac87f2a
domitalk <dominic.m.chu@gmail.com>
2020-09-19 12:37:46 -0400

    underline

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,14 @@
 // get elemenet by ____ and then do something to it
+
+const example = true; 
+example ? "omg" : "it's true";
+
+const exampleArr = [1, 2, 3, 4]
+exampleArr.forEach ( element1 => {
+    exampleArr.forEach( element2 => {
+        element1 == element2 ? true : false; 
+    })
+}); 
+
+
+O(1) > O(log N) > O(N) > O(N^2) > O(2^N)
