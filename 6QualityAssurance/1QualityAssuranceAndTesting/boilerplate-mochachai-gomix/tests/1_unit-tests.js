5826c3c92fd713c4b8fba128e792f49301b59942
Dominic Chu <dominic.m.chu@gmail.com>
2021-02-06 11:31:14 -0500

    match as a regex validator

--- 6QualityAssurance/1QualityAssuranceAndTesting/boilerplate-mochachai-gomix/tests/1_unit-tests.js
+++ 6QualityAssurance/1QualityAssuranceAndTesting/boilerplate-mochachai-gomix/tests/1_unit-tests.js
@@ -190,11 +190,16 @@ suite('Unit Tests', function () {
       assert.include('Arrow', 'row', "Arrow contains row...");
       assert.notInclude('dart', 'queue', "But a dart doesn't contain a queue");
     });
+
+    // match() asserts that the actual value matches the second argument regular expression.
+
+    // Within tests/1_unit-tests.js under the test labelled #15 in the Strings suite, change each assert to either assert.match or assert.notMatch to make the test pass (should evaluate to true). Do not alter the arguments passed to the asserts.
+
     // #15
     test('#match, #notMatch', function () {
       const regex = /^#\sname\:\s[\w\s]+,\sage\:\s\d+\s?$/;
-      assert.fail(formatPeople('John Doe', 35), regex);
-      assert.fail(formatPeople('Paul Smith III', 'twenty-four'), regex);
+      assert.match(formatPeople('John Doe', 35), regex);
+      assert.notMatch(formatPeople('Paul Smith III', 'twenty-four'), regex);
     });
   });
 
