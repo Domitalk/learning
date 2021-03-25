b6d07df5b66c2aeab5066f3bc21e084cef212bee
Dominic Chu <dominic.m.chu@gmail.com>
2021-03-25 17:26:47 -0400

    variable declarations in js

--- 2JavascriptAlogithms/1commentingOut.js
+++ 2JavascriptAlogithms/1commentingOut.js
@@ -1,2 +1,7 @@
 // This is an in-line comment.
 /* This is a multi-line comment */
+
+// declaring variables (the old way)
+var myName; 
+
+// JS has 8 diff datatypes: undefined, null, boolean, string, symbol, bigint, number and object
