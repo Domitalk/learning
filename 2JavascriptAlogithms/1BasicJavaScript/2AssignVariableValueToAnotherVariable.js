43a4a1a828729fdf9d7849aa8f1546c60866eb40
Dominic Chu <dominic.m.chu@gmail.com>
2021-04-05 11:23:31 -0400

    pass through vs copying values

--- 2JavascriptAlogithms/1BasicJavaScript/2AssignVariableValueToAnotherVariable.js
+++ 2JavascriptAlogithms/1BasicJavaScript/2AssignVariableValueToAnotherVariable.js
@@ -5,3 +5,10 @@ var b;
 
 // Only change code below this line
 b = a; 
+// will assign b = 7 
+a = 8;
+// the change 
+console.log(b)
+// b will still show 7 
+
+// when assigning, it will only take the VALUE from a and not link it to a itself
