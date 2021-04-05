3375b56cd3b649add88e4938964c1454495be2f3
Dominic Chu <dominic.m.chu@gmail.com>
2021-04-05 11:26:38 -0400

    explore undefined variable reactions;

--- 2JavascriptAlogithms/1BasicJavaScript/2AssignVariableValueToAnotherVariable.js
+++ 2JavascriptAlogithms/1BasicJavaScript/2AssignVariableValueToAnotherVariable.js
@@ -12,3 +12,21 @@ console.log(b)
 // b will still show 7 
 
 // when assigning, it will only take the VALUE from a and not link it to a itself
+
+var c = 9; 
+// declare and assign at the same time 
+
+
+
+// when declared but not defined, variables come out as undefined 
+// when attempting to do math on undefined, it will result in NaN 
+// when attempting to do string work on undefined, it will force convert undefined into "undfined"
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
