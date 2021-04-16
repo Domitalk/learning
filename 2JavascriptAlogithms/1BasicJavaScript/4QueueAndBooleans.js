d6cb35c0ad7efc881121447f0109496819ae8f99
Dominic Chu <dominic.m.chu@gmail.com>
2021-04-16 15:53:44 -0400

    comparison operators greater than less than

--- 2JavascriptAlogithms/1BasicJavaScript/4QueueAndBooleans.js
+++ 2JavascriptAlogithms/1BasicJavaScript/4QueueAndBooleans.js
@@ -30,7 +30,7 @@ function trueOrFalse(wasThatTrue) {
 // comparison operators == 
 // when different datatype, it will coerce to be able to compare 
 function testEqual(val) {
-  if (val == 12) { // Change this line
+  if (val == 12) { 
     return "Equal";
   }
   return "Not Equal";
@@ -39,7 +39,7 @@ testEqual(10);
 
 // strict equality won't coerce type conversion 
 function testStrict(val) {
-  if (val === 7) { // Change this line
+  if (val === 7) { 
     return "Equal";
   }
   return "Not Equal";
@@ -47,9 +47,75 @@ function testStrict(val) {
 testStrict(10);
 
 function compareEquality(a, b) {
-  if (a === b) { // Change this line
+  if (a === b) { 
     return "Equal";
   }
   return "Not Equal";
 }
 compareEquality(10, "10");
+
+// inequality operator 
+function testNotEqual(val) {
+  if (val != 99) { 
+    return "Not Equal";
+  }
+  return "Equal";
+}
+testNotEqual(10);
+
+// strict inequality operator 
+function testStrictNotEqual(val) {
+  if (val !== 17) { 
+    return "Not Equal";
+  }
+  return "Equal";
+}
+testStrictNotEqual(10);
+
+// greater than operator will convert data types when comparing 
+function testGreaterThan(val) {
+  if (val > 100) {  
+    return "Over 100";
+  }
+  if (val > 10) {  
+    return "Over 10";
+  }
+  return "10 or Under";
+}
+testGreaterThan(10);
+
+// greater or equal 
+function testGreaterOrEqual(val) {
+  if (val >= 20) {  
+    return "20 or Over";
+  }
+  if (val >= 10) {  
+    return "10 or Over";
+  }
+  return "Less than 10";
+}
+testGreaterOrEqual(10);
+
+// less than
+function testLessThan(val) {
+  if (val < 25) {  
+    return "Under 25";
+  }
+  if (val < 55) {  
+    return "Under 55";
+  }
+  return "55 or Over";
+}
+testLessThan(10);
+
+// less than or equal to 
+function testLessOrEqual(val) {
+  if (val <= 12) {  
+    return "Smaller Than or Equal to 12";
+  }
+  if (val <= 24) {  
+    return "Smaller Than or Equal to 24";
+  }
+  return "More Than 24";
+}
+testLessOrEqual(10);
