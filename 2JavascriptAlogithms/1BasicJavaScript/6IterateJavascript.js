57ac50ae8989d33a31cfc0801f630178118b6fe8
Dominic Chu <dominic.m.chu@gmail.com>
2021-04-21 16:07:27 -0400

    basic iterations

---
+++ 2JavascriptAlogithms/1BasicJavaScript/6IterateJavascript.js
@@ -0,0 +1,26 @@
+
+// while loops 
+var myArray = [];
+let i = 5
+while (i > -1) {
+  myArray.push(i)
+  i--
+}
+
+// for loops
+var myArray = [];
+for (let i = 1; i <= 5; i++) {
+  myArray.push(i)
+}
+
+// for loop with a different incrementation 
+var myArray = [];
+for (let i = 1; i <= 9; i += 2) {
+  myArray.push(i)
+}
+
+// increment backwards every other
+var myArray = [];
+for (let i = 9; i > 0; i -=2) {
+  myArray.push(i)
+}
