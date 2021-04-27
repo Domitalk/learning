56ec08e0635e60f5402f644eb2ac01618c8068be
Dominic Chu <dominic.m.chu@gmail.com>
2021-04-27 12:10:00 -0400

    spread operators and rest args

--- 2JavascriptAlogithms/2ES6/Scope.js
+++ 2JavascriptAlogithms/2ES6/Scope.js
@@ -77,3 +77,21 @@ const increment = (number, value) => number + value;
 const increment = (number, value = 1) => {
   return number + value 
 }
+
+// rest args ...args is an array of how many ever args there are 
+const sum = (...args) => {
+  return args.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue 
+  }, 0)
+  // const args = [x, y, z];
+  // return args.reduce((a, b) => a + b, 0);
+}
+
+// spread operators unpack arrays 
+const arr = [6, 89, 3, 45];
+const maximus = Math.max(...arr);
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+arr2 = [...arr1];  
+console.log(arr2);
