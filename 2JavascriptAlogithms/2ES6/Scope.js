7796a89a20a7136df9925e2c40b2f874ed3a0307
Dominic Chu <dominic.m.chu@gmail.com>
2021-06-01 14:00:46 -0400

    destructuring into a different variable name

--- 2JavascriptAlogithms/2ES6/Scope.js
+++ 2JavascriptAlogithms/2ES6/Scope.js
@@ -110,3 +110,16 @@ const HIGH_TEMPERATURES = {
 
 const { today, tomorrow } = HIGH_TEMPERATURES
 
+
+
+// destructuring into different names 
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// const highToday = HIGH_TEMPERATURES.today;
+// const highTomorrow = HIGH_TEMPERATURES.tomorrow;
+
+const { today: highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES
