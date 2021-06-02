d8b6cde460aa71be4247889c71707280bfed1ab0
Dominic Chu <dominic.m.chu@gmail.com>
2021-06-02 11:54:17 -0400

    nested destructuring

--- 2JavascriptAlogithms/2ES6/1Scope.js
+++ 2JavascriptAlogithms/2ES6/1Scope.js
@@ -123,3 +123,17 @@ const HIGH_TEMPERATURES = {
 // const highTomorrow = HIGH_TEMPERATURES.tomorrow;
 
 const { today: highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES
+
+
+// nested destructuring 
+const LOCAL_FORECAST = {
+  yesterday: { low: 61, high: 75 },
+  today: { low: 64, high: 77 },
+  tomorrow: { low: 68, high: 80 }
+};
+
+// const lowToday = LOCAL_FORECAST.today.low;
+// const highToday = LOCAL_FORECAST.today.high;
+
+const { today: { low: lowToday, high: highToday } } = LOCAL_FORECAST
+
